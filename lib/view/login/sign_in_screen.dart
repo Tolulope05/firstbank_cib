6df2f1eaaf0e6ad7be99bd4ee8e3e71627dd44d1
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-03-05 23:02:04 +0100

    fixed overlay option

--- lib/view/login/sign_in_screen.dart
+++ lib/view/login/sign_in_screen.dart
@@ -13,215 +13,217 @@ class SignInScreen extends GetView<SignInViewModel> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        // crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: kToolbarHeight + 50,
-            color: AppColors.primaryColor,
-            width: double.maxFinite,
-            child: const Align(
-              alignment: Alignment.bottomLeft,
-              child: Padding(
-                padding: EdgeInsets.all(24.0),
-                child: Text(
-                  'Logo',
-                  style: TextStyle(
-                    color: AppColors.whiteColor,
-                    fontSize: 24,
-                    fontWeight: FontWeight.w800,
+      // resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: kToolbarHeight + 50,
+              color: AppColors.primaryColor,
+              width: double.maxFinite,
+              child: const Align(
+                alignment: Alignment.bottomLeft,
+                child: Padding(
+                  padding: EdgeInsets.all(24.0),
+                  child: Text(
+                    'Logo',
+                    style: TextStyle(
+                      color: AppColors.whiteColor,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w800,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Container(
-            height: 150,
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            width: double.maxFinite,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage("assets/images/bg_hello.png"),
-              ),
-            ),
-            child: Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: const [
-                    Text(
-                      'Hello',
-                      style: TextStyle(
-                        color: AppColors.blackColor,
-                        fontSize: 24,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    Text(
-                      'Sign in to your X CIB',
-                      style: TextStyle(
-                        color: AppColors.textColor,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ],
-                ),
-                Expanded(
-                  child: Container(),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 12,
-                        vertical: 7,
-                      ),
-                      margin: const EdgeInsets.only(top: 30),
-                      decoration: BoxDecoration(
-                          color: AppColors.whiteColor,
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(60),
-                          ),
-                          boxShadow: [
-                            BoxShadow(
-                              color: AppColors.blackColor.withOpacity(0.05),
-                              blurRadius: 1,
-                              spreadRadius: 1,
-                              offset: const Offset(4, 5), // Shadow position
-                            ),
-                          ]),
-                      child: Row(
-                        children: [
-                          Utils.renderSVGImage(
-                            assetName: 'assets/svg/support.svg',
-                            width: 16.0,
-                            height: 16.0,
-                          ),
-                          const Padding(
-                            padding: EdgeInsets.only(left: 5),
-                            child: Text(
-                              "Need help?",
-                              style: TextStyle(
-                                color: AppColors.textColor,
-                                fontSize: 14,
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              bottom: 12.0,
-              top: 24,
-              right: 24.0,
-              left: 24.0,
-            ),
-            child: AppTextFieldInput(
-              controller: controller.organizationCodeController,
-              headerText: 'Organization Code',
-              hintText: 'Organization code',
-              obscureText: false,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 12.0,
-              horizontal: 24.0,
-            ),
-            child: AppTextFieldInput(
-              controller: controller.usernameController,
-              headerText: 'Username',
-              hintText: 'Corporate ID',
-              obscureText: false,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 12.0,
-              horizontal: 24.0,
-            ),
-            child: AppTextFieldInput(
-              controller: controller.passwordController,
-              headerText: 'Password',
-              hintText: 'Password',
-              obscureText: false,
-              suffixIcon: const Icon(
-                Icons.visibility_off,
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Padding(
-              padding: const EdgeInsets.only(
-                bottom: 40.0,
-                left: 24,
-                top: 12,
-              ),
-              child: GestureDetector(
-                onTap: () {},
-                child: const Text(
-                  "Forgot password?",
-                  style: TextStyle(
-                    color: AppColors.yellowColor2,
-                  ),
+            Container(
+              height: 150,
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              width: double.maxFinite,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/images/bg_hello.png"),
                 ),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: AppButton(
-              onTap: () {},
-              text: "Sign In to your account ",
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              right: 24,
-              left: 24,
-              top: 16,
-            ),
-            child: AppButton(
-              onTap: () {},
-              bgColor: Colors.transparent,
-              textColor: AppColors.primaryColor,
-              // text: "Sign in with face ID",
-              child: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Utils.renderSVGImage(
-                      assetName: 'assets/svg/face_id.svg',
-                      width: 16.0,
-                      height: 16.0,
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(left: 12),
-                      child: Text(
-                        "Sign in with face ID",
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const [
+                      Text(
+                        'Hello',
                         style: TextStyle(
-                          color: AppColors.primaryColor,
-                          fontSize: 16,
+                          color: AppColors.blackColor,
+                          fontSize: 24,
+                          fontWeight: FontWeight.w500,
                         ),
                       ),
-                    )
-                  ],
+                      Text(
+                        'Sign in to your X CIB',
+                        style: TextStyle(
+                          color: AppColors.textColor,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Expanded(
+                    child: Container(),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 12,
+                          vertical: 7,
+                        ),
+                        margin: const EdgeInsets.only(top: 30),
+                        decoration: BoxDecoration(
+                            color: AppColors.whiteColor,
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(60),
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: AppColors.blackColor.withOpacity(0.05),
+                                blurRadius: 1,
+                                spreadRadius: 1,
+                                offset: const Offset(4, 5), // Shadow position
+                              ),
+                            ]),
+                        child: Row(
+                          children: [
+                            Utils.renderSVGImage(
+                              assetName: 'assets/svg/support.svg',
+                              width: 16.0,
+                              height: 16.0,
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.only(left: 5),
+                              child: Text(
+                                "Need help?",
+                                style: TextStyle(
+                                  color: AppColors.textColor,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                bottom: 12.0,
+                top: 24,
+                right: 24.0,
+                left: 24.0,
+              ),
+              child: AppTextFieldInput(
+                controller: controller.organizationCodeController,
+                headerText: 'Organization Code',
+                hintText: 'Organization code',
+                obscureText: false,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 12.0,
+                horizontal: 24.0,
+              ),
+              child: AppTextFieldInput(
+                controller: controller.usernameController,
+                headerText: 'Username',
+                hintText: 'Corporate ID',
+                obscureText: false,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 12.0,
+                horizontal: 24.0,
+              ),
+              child: AppTextFieldInput(
+                controller: controller.passwordController,
+                headerText: 'Password',
+                hintText: 'Password',
+                obscureText: false,
+                suffixIcon: const Icon(
+                  Icons.visibility_off,
                 ),
               ),
             ),
-          ),
-        ],
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 40.0,
+                  left: 24,
+                  top: 12,
+                ),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: const Text(
+                    "Forgot password?",
+                    style: TextStyle(
+                      color: AppColors.yellowColor2,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: AppButton(
+                onTap: () {},
+                text: "Sign In to your account ",
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                right: 24,
+                left: 24,
+                top: 16,
+              ),
+              child: AppButton(
+                onTap: () {},
+                bgColor: Colors.transparent,
+                textColor: AppColors.primaryColor,
+                // text: "Sign in with face ID",
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Utils.renderSVGImage(
+                        assetName: 'assets/svg/face_id.svg',
+                        width: 16.0,
+                        height: 16.0,
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(left: 12),
+                        child: Text(
+                          "Sign in with face ID",
+                          style: TextStyle(
+                            color: AppColors.primaryColor,
+                            fontSize: 16,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
