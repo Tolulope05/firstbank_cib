3428642e32e30db88bbd5a5e0636e2fdbd43188d
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-03-06 22:50:05 +0100

    going to more screen

--- lib/view/insights/insights_screen.dart
+++ lib/view/insights/insights_screen.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 
+import '../../constants/colors.dart';
+
 class InsightsScreen extends StatelessWidget {
   const InsightsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: AppColors.whiteColor2,
+        title: const Text(
+          "Transfer",
+          style: TextStyle(
+            color: AppColors.blackColor,
+            fontSize: 24,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: const [
--- lib/view/more/more_screen.dart
+++ lib/view/more/more_screen.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 
+import '../../constants/colors.dart';
+
 class MoreScreen extends StatelessWidget {
   const MoreScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: AppColors.whiteColor2,
+        title: const Text(
+          "More",
+          style: TextStyle(
+            color: AppColors.blackColor,
+            fontSize: 24,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          Center(
-            child: Text("More"),
-          )
-        ],
+        children: const [],
       ),
     );
   }
