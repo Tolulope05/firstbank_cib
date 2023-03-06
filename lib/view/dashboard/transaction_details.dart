8b6ba9c785abb3ba946bec4968941d42182a5970
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-03-06 16:06:46 +0100

    new fix

---
+++ lib/view/dashboard/transaction_details.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TransactionDetails extends StatelessWidget {
+  const TransactionDetails({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
