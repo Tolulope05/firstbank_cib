33047ede0136f24871b30afc1815d6e0418e1904
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-03-10 02:49:42 +0100

    trnasfer screen model

--- lib/view_model/transfer_screen_view_model.dart
+++ lib/view_model/transfer_screen_view_model.dart
@@ -1,7 +1,11 @@
+import 'package:firstbank_cib/services/services.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class TransferScreenViewModel extends GetxController {
+import '../model/payment_type_response.dart';
+import '../services/payment_services.dart';
+
+class TransferScreenViewModel extends GetxController with CacheManager {
 // own account number
   TextEditingController sourceAccountController = TextEditingController();
   TextEditingController beneficialAccountController = TextEditingController();
@@ -16,4 +20,35 @@ class TransferScreenViewModel extends GetxController {
       TextEditingController();
   TextEditingController firstBankAmountAccountControllerMemo =
       TextEditingController();
+
+  // local payment servie call
+  final Rx<LocalPaymentResponse> _localPaymentResponse =
+      LocalPaymentResponse().obs;
+  LocalPaymentResponse get localPaymentResponse => _localPaymentResponse.value;
+
+  Paymentservices paymentservices = Paymentservices();
+
+  @override
+  void onInit() async {
+    super.onInit();
+    await getLocalPayment();
+  }
+
+  //call get local payment
+  Future<void> getLocalPayment() async {
+    LocalPaymentResponse localPayResp = await paymentservices.getLocalPayment(
+      session: "session",
+      username: "${getFullname()}@${getCorporateCode()}",
+      subsidiaryId: 2,
+    );
+    if (localPayResp.success == true) {
+      _localPaymentResponse.value = localPayResp;
+    } else {
+      Get.snackbar(
+        "Unable to fetch data",
+        localPayResp.message.toString(),
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
+  }
 }
